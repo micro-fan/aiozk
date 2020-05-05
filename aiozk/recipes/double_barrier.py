28c4500b4a8613937f1f43c72f62330d48e1c8aa
Junyeong Jeong <rhdxmr@gmail.com>
2020-05-05 17:49:08 +0900

    barrier: Refactoring DoubleBarrier.leave method

--- aiozk/recipes/double_barrier.py
+++ aiozk/recipes/double_barrier.py
@@ -9,6 +9,7 @@ log = logging.getLogger(__name__)
 
 
 class DoubleBarrier(SequentialRecipe):
+    ZNODE_LABEL = 'worker'
 
     def __init__(self, base_path, min_participants):
         super().__init__(base_path)
@@ -20,14 +21,12 @@ class DoubleBarrier(SequentialRecipe):
 
     async def enter(self, timeout=None):
         log.debug("Entering double barrier %s", self.base_path)
-        barrier_lifted = self.client.wait_for_events(
-            [WatchEvent.CREATED], self.sentinel_path
-        )
+        barrier_lifted = self.client.wait_for_events([WatchEvent.CREATED],
+                                                     self.sentinel_path)
 
         exists = await self.client.exists(path=self.sentinel_path, watch=True)
 
-        ZNODE_LABEL = 'worker'
-        await self.create_unique_znode(ZNODE_LABEL)
+        await self.create_unique_znode(self.ZNODE_LABEL)
 
         if exists:
             return
@@ -47,24 +46,27 @@ class DoubleBarrier(SequentialRecipe):
             except asyncio.TimeoutError:
                 raise exc.TimeoutError
         except Exception:
-            await self.delete_unique_znode_retry(ZNODE_LABEL)
+            await self.delete_unique_znode_retry(self.ZNODE_LABEL)
             raise
 
     async def leave(self, timeout=None):
         log.debug("Leaving double barrier %s", self.base_path)
-        owned_positions, participants = await self.analyze_siblings()
-        while len(participants) > 1:
-            if owned_positions["worker"] == 0:
+
+        while True:
+            owned_positions, participants = await self.analyze_siblings()
+            if not participants:
+                return
+
+            if len(participants) == 1:
+                await self.delete_unique_znode(self.ZNODE_LABEL)
+                try:
+                    await self.client.delete(self.sentinel_path)
+                except exc.NoNode:
+                    pass
+                return
+
+            if owned_positions[self.ZNODE_LABEL] == 0:
                 await self.wait_on_sibling(participants[-1], timeout)
             else:
-                await self.delete_unique_znode("worker")
+                await self.delete_unique_znode(self.ZNODE_LABEL)
                 await self.wait_on_sibling(participants[0], timeout)
-
-            owned_positions, participants = await self.analyze_siblings()
-
-        if len(participants) == 1 and "worker" in owned_positions:
-            await self.delete_unique_znode("worker")
-            try:
-                await self.client.delete(self.sentinel_path)
-            except exc.NoNode:
-                pass
