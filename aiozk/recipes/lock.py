be3607f8983405ac4673971a0f224a86aae7c970
Junyeong Jeong <rhdxmr@gmail.com>
2020-07-11 21:28:36 +0900

    lock: Support general lock API
    An async context manager, .acquire/.release pair and .locked methods are added
    to Lock.
    
    Reviewed-by: Kirill Pinchuk <cybergrind@gmail.com>

--- aiozk/recipes/lock.py
+++ aiozk/recipes/lock.py
@@ -1,16 +1,48 @@
-from aiozk import exc, Deadline
-
+from .. import exc
+from ..deadline import Deadline
 from .base_lock import BaseLock
 
 
 class Lock(BaseLock):
+    def __init__(self, base_path, znode_label='lock', blocked_by=None):
+        super().__init__(base_path)
+        self.znode_label = znode_label
+        self.blocked_by = blocked_by
+        self.is_locked = False
+        # This lock is for coordination between processes through network. Not
+        # for tasks in a process. Simultaneous .acquire calls is misuse.
+        self.in_use = False
+
+    async def __aenter__(self):
+        await self.acquire()
+
+    async def __aexit__(self, exc_type, exception, tb):
+        await self.release()
 
     async def acquire(self, timeout=None):
+        if self.in_use:
+            raise RuntimeError('lock is already in use')
+        self.in_use = True
         deadline = Deadline(timeout)
-        result = None
-        while not result:
+        while not self.is_locked and not deadline.has_passed:
             try:
-                result = await self.wait_in_line("lock", deadline.timeout)
+                await self.wait_in_line(self.znode_label,
+                                        deadline.timeout,
+                                        blocked_by=self.blocked_by)
             except exc.SessionLost:
                 continue
-        return result
+            except Exception as e:
+                self.in_use = False
+                raise e from None
+
+            self.is_locked = True
+
+    async def release(self):
+        try:
+            await self.get_out_of_line(self.znode_label)
+        finally:
+            self.is_locked = False
+            self.in_use = False
+
+    def locked(self):
+        return self.is_locked
