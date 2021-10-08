c032770b11daf91ed2fe6de1aa7abc05ab557992
Kirill Pinchuk <cybergrind@gmail.com>
2021-10-08 18:31:01 +0300

    python 3.10 support

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: Implementation',
         'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Software Development',
